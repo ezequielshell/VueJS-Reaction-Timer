389a15a963374f448f76d3c572fa10729c002ca7
Ezequiel <ezequiel.h@lyncas.net>
2023-11-06 14:17:02 -0300

    Project Commit

---
+++ src/components/Result.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>Reaction Time - {{ scoreRes }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+<script>
+export default {
+  props: ["scoreRes"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.scoreRes < 250) {
+      this.rank = "Fast Fingers";
+    } else if (this.scoreRes < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "Slow One...";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
